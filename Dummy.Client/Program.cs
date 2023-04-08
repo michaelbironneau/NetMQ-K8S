b9e49842e375a515998d9233d808b9d59893c7dd
Michael Bironneau <mbironneau@gmail.com>
2023-04-08 16:59:55 +0100

    Initial commit without K8S

---
+++ Dummy.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+
+/**
+---------------------
+Greeting Client
+---------------------
+Requests a greeting for a given name (eg Ben) and receives the response.
+**/
+const string GreetedName = "Ben";
+
+using (var greetingClient = new RequestSocket())
+{
+    
+    greetingClient.Connect("tcp://localhost:5555");
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine("Sending '{0}'", GreetedName);
+        greetingClient.SendFrame(GreetedName);
+        var greeting = greetingClient.ReceiveFrameString();
+        Console.WriteLine("Received greeting: {0}", greeting);
+    }
+}
---
+++ Dummy.Server/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using NetMQ;
+using NetMQ.Sockets;
+
+/**
+---------------------
+Greeting Service
+---------------------
+Receives a request containing <name> and replies with "Hello, <name>"
+**/
+using (var server = new ResponseSocket())
+{
+    server.Bind("tcp://*:5555");
+    while (true)
+    {
+        var name = server.ReceiveFrameString();
+        Console.WriteLine("Received name: {0}", name);
+        // simulate some processing time
+        Thread.Sleep(100);
+        Console.WriteLine("Sending 'Hello, {0}'", name);
+        server.SendFrame("Hello, " + name);
+    }
+}
